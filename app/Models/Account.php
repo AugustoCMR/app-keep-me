9bbfdc131b1c0bb64863a178617ea51795e932af
Augusto Ribeiro <augustoribeiro100.g.j@gmai.com>
2025-06-02 20:02:58 -0300

    feat: create migration account_user

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -11,4 +11,14 @@ class Account extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'type', 'color', 'amount'];
+
+    public function users()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'account_user',
+            'account_id',
+            'user_id'
+        )->withTimestamps();
+    }
 }
